04cd9970871f5e7f4b88366c75a4a52bcf251be7
Matt Bridges <Matthew.Bridges1@gmail.com>
2015-04-07 01:42:10 -0400

    Added Camera Controller Script base.  Bullets are currently broken, due to spawning behind new camera position.  Will fix tomorrow

---
+++ Save Earth/Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    private Vector2 moveTemp;
+
+    private Transform player;
+    
+    public float speed = 5;
+
+  
+    public float xDif, yDif;
+
+ 
+    public float movementThreshold = 3;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+	void Update () {
+        
+        
+        if (player != null)
+        {
+            if (player.transform.position.x > transform.position.x)
+                xDif = player.transform.position.x - transform.position.x;
+            else
+                xDif = transform.position.x - player.transform.position.x;
+            if (player.transform.position.y > transform.position.y)
+                yDif = player.transform.position.y - transform.position.y;
+            else
+                yDif = transform.position.y - player.position.y;
+
+
+            if (xDif >= movementThreshold || yDif >= movementThreshold || xDif <= -movementThreshold || yDif <= -movementThreshold)
+            {
+                moveTemp = player.transform.position;
+            }
+
+
+            transform.position = Vector2.MoveTowards(transform.position, moveTemp, speed * Time.deltaTime);
+        }
+    
+        
+	}
+}
