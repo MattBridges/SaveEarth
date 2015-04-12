f7ec58b7cf709b9b93ca4d10051ddb7e1f2850be
KhorelisSylvari <Khorelis@fullsail.edu>
2015-04-12 13:23:26 -0500

    Forgot to commit the dragonfly script

--- Save Earth/Assets/Scripts/Controllers/DragonflyShip.cs
+++ Save Earth/Assets/Scripts/Controllers/DragonflyShip.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DragonflyShip : MonoBehaviour {
+public class DragonflyShip : AIController {
+
+	//private AIController controller;
+	public float moveSpeed;
+	public int health;
+	//public Ship thisShip;
 
     // Use this for initialization
-    void Start () {
+    public override void Start () {
 
+	//	thisShip = new Ship(moveSpeed, health);	
+		base.Start ();
+		currentState = AIstate.AI_Follow;
     }
 
     // Update is called once per frame
