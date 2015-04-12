add72c7435c8fc24e1c82d07b4b562c51b5081ac
KhorelisSylvari <Khorelis@fullsail.edu>
2015-04-12 13:18:42 -0500

    Added base Follow AI for now

---
+++ Save Earth/Assets/Scripts/Controllers/AIController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIController : MonoBehaviour {
+	
+	public enum AIstate { AI_Idle, AI_Follow, AI_Retreat, AI_Stationary };
+	public AIstate currentState;
+	public bool attack = false;
+	public GameObject pShip;
+	
+	// Use this for initialization
+	public virtual void Start () 
+	{
+		Debug.Log ("Called base");
+	}
+
+	void OnEnable()
+	{
+		pShip = GameObject.FindGameObjectWithTag ("Player");
+
+		if (!pShip) 
+		{
+			Debug.Log ("Error: No player ship found");
+		}
+	}
+
+	public virtual void AIFollow()
+	{
+		if (pShip) 
+		{
+			if (Vector3.Distance (transform.position, pShip.transform.position) > 2)
+				transform.position = Vector3.Lerp (transform.position, pShip.transform.position, Time.fixedDeltaTime);
+		}
+	}
+
+	public virtual void AIRetreat()
+	{
+
+	}
+	
+	public virtual void AIAttack()
+	{
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		switch (currentState) 
+		{
+			case AIstate.AI_Idle:
+				break;
+			case AIstate.AI_Follow:
+				AIFollow();
+				AIAttack();
+				break;
+			case AIstate.AI_Retreat:
+				AIRetreat();
+				AIAttack();
+				break;
+			case AIstate.AI_Stationary:
+				AIAttack();
+				break;
+			default:
+				break;
+		}
+	}
+}
