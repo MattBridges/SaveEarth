2246769338f2af6b6c9d59c79224ffb181a371e3
KhorelisSylvari <Khorelis@fullsail.edu>
2015-05-21 21:41:02 -0500

    Added Enemy Base Ship

---
+++ Save Earth/Assets/Scripts/Controllers/EnemyBaseShip.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBaseShip : AIController {
+
+	public bool canWake;
+	
+//	public Sprite[] shipSprites;
+//	private SpriteRenderer sprite;
+	
+	// Use this for initialization
+	public override void Start () 
+	{
+		base.Start ();
+		attack = false;
+		health = 500;
+//		weaponShotPosition = transform.FindChild ("DragonflyCannon").gameObject.transform;
+//		target = pShip;
+		
+//		sprite = this.gameObject.GetComponent<SpriteRenderer>();
+//		shipSprites = Resources.LoadAll<Sprite>("Ships/RedShips/RD2");
+	}
+	
+	public override void AIFollow()
+	{
+		base.AIFollow();
+
+	}
+	
+	public override void AIRetreat()
+	{
+		base.AIRetreat();
+	}
+	
+	public override void AIAttack()
+	{
+		
+	}
+	
+	public override void AIStationary()
+	{
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "PlayerBullet") {
+			TakeDamage(5);
+		}		
+	}
+	
+	public override void FixedUpdate ()
+	{
+		base.FixedUpdate();
+		
+//		if (hasMothershipShield)
+//			sprite.sprite = shipSprites[1];
+//		else
+//			sprite.sprite = shipSprites[0];
+	}
+	
+	void OnDisable()
+	{
+		health = maxHealth;
+	}
+}
