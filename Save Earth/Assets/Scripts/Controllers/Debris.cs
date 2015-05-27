87d6f902a555230fe8ae2f579f6c0dede8b1d253
KhorelisSylvari <Khorelis@fullsail.edu>
2015-05-26 21:38:16 -0500

    Changed DebrisManager and Debris, Also modified the Orbital Base prefab and set bullets to destroy on collision with debris

---
+++ Save Earth/Assets/Scripts/Controllers/Debris.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Debris : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	private SpriteRenderer sprite;
+	private float debrisLife;
+	private float tempTime;
+	private int health;
+	public int maxHealth;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		rb = this.gameObject.GetComponent<Rigidbody2D>();
+		sprite = this.gameObject.GetComponent<SpriteRenderer>();
+		health = maxHealth;
+	}
+	
+	void OnEnable()
+	{
+		tempTime = Time.time;
+	}
+	
+	public void RandomizeDebris(float lowRange, float hiRange, float lifetime, float speed)
+	{
+		debrisLife = lifetime;
+		
+		Vector3 loc = new Vector3(-50, Random.Range(lowRange, hiRange), 0);
+		transform.position = loc;
+		sprite.sprite = DebrisManager.Instance.debrisSprites[Random.Range(0, DebrisManager.Instance.debrisSprites.Length)];
+
+		float scale = Random.Range(0.25f, 2.0f);
+		int rot = Random.Range (0, 360);
+		
+		transform.localScale = new Vector3(scale, scale, scale);
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, rot));
+		this.gameObject.SetActive(true);
+		Destroy(this.gameObject.GetComponent<PolygonCollider2D>());		
+		PolygonCollider2D pC = this.gameObject.AddComponent<PolygonCollider2D>();
+		pC.isTrigger = true;	
+		rb.AddForce(new Vector3(speed, 0, 0), ForceMode2D.Impulse);
+		rb.AddTorque(Random.Range(-2.5f, 2.5f), ForceMode2D.Impulse);
+	}
+	
+	void OnDisable()
+	{
+		health = maxHealth;
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "PlayerBullet")
+		{
+			health -= 5;
+			
+			if (health <= 0)
+				this.gameObject.SetActive(false);
+		}	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if ((Time.time - tempTime) > debrisLife)
+		{
+			this.gameObject.SetActive (false);
+			tempTime = 0f;
+		}
+	}
+}
--- Save Earth/Assets/Scripts/Managers/DebrisManager.cs
+++ Save Earth/Assets/Scripts/Managers/DebrisManager.cs
@@ -16,8 +16,7 @@ public class DebrisManager : MonoBehaviour {
 	[HideInInspector]
 	public Sprite[] debrisSprites;
 
-	private List<GameObject> debrisList;
-	private Dictionary<float, GameObject> activeDebris;
+	private List<Debris> debrisList;
 	private static DebrisManager _instance;
 	private float lastTime;
 	
@@ -38,8 +37,7 @@ public class DebrisManager : MonoBehaviour {
 	void Start () 
 	{
 		debrisSprites = Resources.LoadAll<Sprite>(debris.name);		
-		debrisList = new List<GameObject>();
-		activeDebris = new Dictionary<float, GameObject>();
+		debrisList = new List<Debris>();
 	
 		InitializeDebris();
 	}
@@ -49,21 +47,24 @@ public class DebrisManager : MonoBehaviour {
 		for (int i = 0; i < spawnCount; i++)
 		{
 			GameObject go = GameObject.Instantiate<GameObject>(debris) as GameObject;
-			debrisList.Add(go);
+			
+			debrisList.Add(go.GetComponent<Debris>());
 			go.transform.SetParent(transform);
 			go.SetActive(false);
 		}	
 	}
 	
-	private GameObject GetInactiveDebris()
+	private Debris GetInactiveDebris()
 	{
-		foreach (GameObject go in debrisList)
+		foreach (Debris go in debrisList)
 		{
-			if (!go.activeSelf)
+			if (!go.gameObject.activeSelf)
 				return go;
 		}
 		
-		return GameObject.Instantiate<GameObject>(debris) as GameObject;
+		GameObject deb = GameObject.Instantiate<GameObject>(debris) as GameObject;
+		
+		return deb.GetComponent<Debris>();
 	}
 	
 	// Update is called once per frame
@@ -75,37 +76,8 @@ public class DebrisManager : MonoBehaviour {
 			{
 				lastTime = Time.time;
 				
-				Vector3 loc = new Vector3(-50, Random.Range(lowRange, hiRange), 0);
-				GameObject sendDebris = GetInactiveDebris();
-				
-				Destroy(sendDebris.GetComponent<PolygonCollider2D>());
-				
-				sendDebris.transform.position = loc;
-				sendDebris.GetComponent<SpriteRenderer>().sprite = debrisSprites[Random.Range(0, debrisSprites.Length)];
-				
-				PolygonCollider2D pC = sendDebris.AddComponent<PolygonCollider2D>();
-				pC.isTrigger = true;
-				
-				float scale = Random.Range(0.25f, 2.0f);
-				int rot = Random.Range (0, 360);
-				
-				sendDebris.transform.localScale = new Vector3(scale, scale, scale);
-				sendDebris.transform.rotation = Quaternion.Euler(new Vector3(0, 0, rot));
-				
-				sendDebris.SetActive(true);
-				
-				sendDebris.GetComponent<Rigidbody2D>().AddForce(new Vector3(debrisSpeed, 0, 0), ForceMode2D.Impulse);
-				activeDebris.Add(Time.time, sendDebris);
-			}
-			
-			foreach (KeyValuePair<float, GameObject> keyValue in activeDebris)
-			{
-				if ((Time.time - keyValue.Key) > lifetime)
-				{
-					keyValue.Value.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
-					keyValue.Value.SetActive(false);
-					activeDebris.Remove(keyValue.Key);
-				}
+				Debris sendDebris = GetInactiveDebris();
+				sendDebris.RandomizeDebris(lowRange, hiRange, lifetime, debrisSpeed);
 			}
 		}
 	}
